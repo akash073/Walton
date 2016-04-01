5b202bce719d83c716c30824eb92aa027b84d464
akash073 <akash073@gmail.com>
2016-04-01 20:28:27 +0600

    SUCCESSFULLY FETCHED DATA BY CASSANDRA RESULTSET

--- cassandra01/src/main/java/App.java
+++ cassandra01/src/main/java/App.java
@@ -1,6 +1,10 @@
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+
+import java.util.List;
+
 
 /**
  * Created by Mhamudul on 01/04/2016.
@@ -16,9 +20,13 @@ public class App {
                .build();
 
        Session session = cluster.connect(keyspace);
-       String cqlStatement = "SELECT * FROM employees ";
-       for (Row row : session.execute(cqlStatement)) {
-           System.out.println(row.toString());
+       String cqlStatement = "SELECT * FROM messages";
+       ResultSet results=session.execute(cqlStatement);
+       List<Row> rows = results.all();
+       for (Row row : rows) {
+          // Row row = row.;
+           System.out.println(row.getLong("id")+"->"+row.getString("message")+"->"+row.getObject("sender")+"->"+row.getObject("timestamp"));
+          // System.out.println(row.getString(1));
        }
     }
 
