97891e6be8411e8205f2b78ae49d4185f66747c0
akash073 <akash073@gmail.com>
2016-04-12 14:54:52 +0600

    RDLC Report pdf  for button click download

---
+++ StackOverflow_Solve/StackOverflow_Solve/RDLCReport/TestPage.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestPage.aspx.cs" Inherits="StackOverflow_Solve.RDLCReport.TestPage" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+    Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <iframe id="frmPrint" name="IframeName" width="500" 
+  height="200" runat="server" 
+  style="display: none" runat="server"></iframe>
+  <asp:ImageButton ID="btnPrint" runat="server" onclick="btnPrint_Click"   ImageUrl="~/RDLCReport/print_button.png" />
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" 
+            Font-Size="8pt" InteractiveDeviceInfos="(Collection)" 
+            WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" ShowPrintButton="true">
+            <LocalReport ReportPath="RDLCReport\Report1.rdlc">
+            </LocalReport>
+        </rsweb:ReportViewer>
+        <br />
+    </div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+    </form>
+</body>
+</html>
---
+++ StackOverflow_Solve/StackOverflow_Solve/RDLCReport/TestPage.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Reporting.WebForms;
+using PrintReportSample;
+
+namespace StackOverflow_Solve.RDLCReport
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //ReportPrintDocument rp = new ReportPrintDocument(ReportViewer1.ServerReport);
+           // rp.Print();  
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Warning[] warnings;
+            string[] streamIds;
+            string mimeType = string.Empty;
+            string encoding = string.Empty;
+            string extension = string.Empty;
+            string HIJRA_TODAY = "01/10/1435";
+            ReportParameter[] param = new ReportParameter[3];
+            param[0] = new ReportParameter("CUSTOMER_NUM", "akash");
+            param[1] = new ReportParameter("REF_CD", "batash");
+            param[2] = new ReportParameter("HIJRA_TODAY", HIJRA_TODAY);
+
+            byte[] bytes = ReportViewer1.LocalReport.Render(
+                "PDF",
+                null,
+                out mimeType,
+                out encoding,
+                out extension,
+                out streamIds,
+                out warnings);
+
+            Response.Buffer = true;
+            Response.Clear();
+            Response.ContentType = mimeType;
+            Response.AddHeader(
+                "content-disposition",
+                "attachment; filename= filename" + "." + extension);
+            Response.OutputStream.Write(bytes, 0, bytes.Length); // create the file  
+            Response.Flush(); // send it to the client to download  
+            Response.End();
+        }
+
+        protected void btnPrint_Click(object sender, ImageClickEventArgs e)
+        {
+            Warning[] warnings;
+            string[] streamids;
+            string mimeType;
+            string encoding;
+            string extension;
+
+            byte[] bytes = ReportViewer1.LocalReport.Render("PDF", null, out mimeType,
+                           out encoding, out extension, out streamids, out warnings);
+
+            FileStream fs = new FileStream(HttpContext.Current.Server.MapPath("output.pdf"),
+            FileMode.Create);
+            fs.Write(bytes, 0, bytes.Length);
+            fs.Close();
+
+            //Open existing PDF
+            Document document = new Document(PageSize.LETTER);
+            PdfReader reader = new PdfReader(HttpContext.Current.Server.MapPath("output.pdf"));
+            //Getting a instance of new PDF writer
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(
+               HttpContext.Current.Server.MapPath("Print.pdf"), FileMode.Create));
+            document.Open();
+            PdfContentByte cb = writer.DirectContent;
+
+            int i = 0;
+            int p = 0;
+            int n = reader.NumberOfPages;
+            Rectangle psize = reader.GetPageSize(1);
+
+            float width = psize.Width;
+            float height = psize.Height;
+
+            //Add Page to new document
+            while (i < n)
+            {
+                document.NewPage();
+                p++;
+                i++;
+
+                PdfImportedPage page1 = writer.GetImportedPage(reader, i);
+                cb.AddTemplate(page1, 0, 0);
+            }
+
+            //Attach javascript to the document
+            PdfAction jAction = PdfAction.JavaScript("this.print(true);\r", writer);
+            writer.AddJavaScript(jAction);
+            document.Close();
+
+            //Attach pdf to the iframe
+            frmPrint.Attributes["src"] = "Print.pdf";
+        }
+    }
+}
