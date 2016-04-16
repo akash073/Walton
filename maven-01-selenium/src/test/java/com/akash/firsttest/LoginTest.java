4d989287eaa9e4710725e0d4592fed3a7f471d87
akash073 <akash073@gmail.com>
2016-04-16 17:54:43 +0600

    ASP.net MVC solves in visual Studio 2013 with MVC 5

--- maven-01-selenium/src/test/java/com/akash/firsttest/LoginTest.java
+++ maven-01-selenium/src/test/java/com/akash/firsttest/LoginTest.java
@@ -6,13 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //https://www.youtube.com/watch?v=VZxpv5l5JvY
 public class LoginTest {
-@Test
-    public void startLoginPage(){
-    WebDriver driver=new FirefoxDriver();
-    driver.navigate().to("http://localhost:8080/");
-    Assert.assertTrue("Sign In",driver.getTitle().startsWith("HI"));
-  //  driver.close();
-    driver.quit();
-}
+
+    @Test
+    public void startLoginPage()
+    {
+        WebDriver driver=new FirefoxDriver();
+        driver.navigate().to("http://localhost:8080/");
+        Assert.assertTrue("Sign In",driver.getTitle().startsWith("HI"));
+      //  driver.close();
+        driver.quit();
+    }
 
 }
