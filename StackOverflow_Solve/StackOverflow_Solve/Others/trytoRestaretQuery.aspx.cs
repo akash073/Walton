acc5319fd6c456d8e03215395635038145d3f019
akash073 <akash073@gmail.com>
2016-04-20 14:01:10 +0600

    Maven with struts2 . Generated by 1.Create Mvn Project 2.Add Strut2 dependency 3.Follow http://www.javatpoint.com/steps-to-create-struts-2-application-example

---
+++ StackOverflow_Solve/StackOverflow_Solve/Others/trytoRestaretQuery.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text.pdf;
+
+namespace StackOverflow_Solve.Others
+{
+    public partial class trytoRestaretQuery : System.Web.UI.Page
+    {
+        static int noOfTries = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            function();
+        }
+
+        private void function()
+        {
+          
+            try
+            {
+                
+                if (noOfTries == 10) goto XX;
+                noOfTries++;
+                int a = 0;
+                int b = 1 / a;
+               
+                
+            }
+            catch (Exception ew)
+            {
+                Response.Write(ew.Message + "<br/>");
+                function();
+
+            }
+            XX:
+            int c = 0;
+        }
+    }
+}
