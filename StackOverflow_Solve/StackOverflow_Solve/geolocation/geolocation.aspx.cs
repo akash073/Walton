e89ea192f3d39721f9fd0e014b354f2bd274a40e
akash073 <akash073@gmail.com>
2016-05-05 11:50:09 +0600

    LARAVEL commit

---
+++ StackOverflow_Solve/StackOverflow_Solve/geolocation/geolocation.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StackOverflow_Solve.geolocation
+{
+    public partial class geolocation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string apiUrl = http://freegeoip.net/xml/{ip}
+
+HttpClient HttpClient = new HttpClient();
+
+var response = HttpClient.GetAsync(apiUrl).Result;
+
+if (response != null && response.ReasonPhrase != "Unauthorized")
+ {
+   var myobject = response.Content.ReadAsStringAsync();
+
+ }
+        }
+    }
+}
