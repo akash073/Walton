92d71cc0f5146f6053f50d5408e495b540e39c26
akash073 <akash073@gmail.com>
2016-05-15 13:41:11 +0600

    Tried with following link what is necessary i want http://stackoverflow.com/questions/28055287/asp-net-mvc-5-group-of-radio-buttons

---
+++ Mvc5Practice/Mvc5Practice/Controllers/RadioButtonListController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5Practice.ViewModels;
+using Mvc5Practice.ViewModels.QuestionAnswer;
+
+namespace Mvc5Practice.Controllers
+{
+    public class RadioButtonListController : Controller
+    {
+        // GET: RadioButtonList
+        public ActionResult Index()
+        {
+            var vm = new CheckOutViewModel();
+            vm.SelectedPaymentType = "test";
+            var selectItemses=new List<SelectItems>();
+            selectItemses.Add(new SelectItems
+            {
+                option1 = "a",
+                option2 = "b",
+                totalOption = 2
+            });
+            
+            vm.PaymentTypes = selectItemses; //gets a list of SelectItems
+            return View(vm);
+        }
+        [HttpPost]
+        public ActionResult Index(CheckOutViewModel model)
+        {
+            //check the value of model.SelectedPaymentType
+            int a =0;
+            return View();
+        }
+
+        public ActionResult Question()
+        {
+            StudentVM model = new StudentVM();
+model.ID = 1;
+model.Name = "bambiinela";
+model.Subjects = new List<SubjectVM>()
+{
+  new SubjectVM()
+  {
+    Questions = new List<QuestionVM>()
+    {
+      new QuestionVM()
+      {
+        ID = 1,
+        Text = "Question 1",
+       // SelectedAnswer = 1, // set this if you want to preselect an option
+        PossibleAnswers = new List<AnswerVM>()
+        {
+          new AnswerVM()
+          {
+            ID = 1,
+            Text = "Answer A"
+          },
+          new AnswerVM()
+          {
+            ID = 1,
+            Text = "Answer B"
+          }
+        }
+      },
+      new QuestionVM()
+      {
+        ID = 2,
+        Text = "Question 2",
+        PossibleAnswers = new List<AnswerVM>()
+        {
+          // similar to above
+          new AnswerVM()
+          {
+            ID = 1,
+            Text = "Answer c"
+          },
+          new AnswerVM()
+          {
+            ID = 1,
+            Text = "Answer d"
+          }
+        }
+      }
+    }
+  },
+  new SubjectVM()
+  {
+    ID = 1,
+    Name = "Math",
+    Questions = new List<QuestionVM>()
+    {
+      //// similar to above
+      //new QuestionVM
+      //{
+      
+      //}
+    }
+  }
+};
+            return View();
+        }
+
+        
+    }
+}
---
+++ Mvc5Practice/Mvc5Practice/ViewModels/CheckOutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5Practice.ViewModels
+{
+    public class CheckOutViewModel
+    {
+        public string SelectedPaymentType { set; get; }
+        public IEnumerable<SelectItems> PaymentTypes { set; get; }
+    }
+}
