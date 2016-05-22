a58a06cfa56f17aed2f1e7dcb94498fead72ab8c
akash073 <akash073@gmail.com>
2016-05-22 12:04:41 +0600

    TEst

---
+++ Mvc5Practice/WsmsQuiz/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WsmsQuiz.ViewModels
+{
+    public class QuestionViewModel
+    {
+        public string QuestionName { get; set; }
+        public List<int> QuestionOption { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
