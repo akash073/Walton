f9e7b917f9789135a3e01d2844f3f96836bdd8b2
akash073 <akash073@gmail.com>
2016-05-26 09:23:53 +0600

    WCF Hosting by console powered by codeproject

---
+++ WCF_Self_Hosting/MyMathServiceHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyMathServiceHost
+{
+    class Program
+    {
+        //http://www.codeproject.com/Articles/650869/Creating-a-Self-Hosted-WCF-Service
+        static void Main(string[] args)
+        {
+            ServiceHost svcHost = null;
+            try
+            {
+                svcHost = new ServiceHost(typeof(MyMathServiceLib.MyMathService));
+                svcHost.Open(); Console.WriteLine("\n\nService is Running  at following address");
+                Console.WriteLine("\nhttp://localhost:9001/MyMathService");
+                Console.WriteLine("\nnet.tcp://localhost:9002/MyMathService");
+            }
+            catch (Exception eX)
+            {
+                svcHost = null;
+                Console.WriteLine("Service can not be started \n\nError Message [" + eX.Message + "]");
+            } if (svcHost != null)
+            {
+                Console.WriteLine("\nPress any key to close the Service");
+                Console.ReadKey();
+                svcHost.Close();
+                svcHost = null;
+            }       
+        }
+    }
+}
---
+++ WCF_Self_Hosting/MyMathServiceLib/IMyMathService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyMathServiceLib
+{
+    [ServiceContract]  
+    public interface IMyMathService
+    {
+        [OperationContract]
+        double Add(double dblNum1, double dblNum2);
+        [OperationContract]
+        double Subtract(double dblNum1, double dblNum2);
+        [OperationContract]
+        double Multiply(double dblNum1, double dblNum2);
+        [OperationContract]
+        double Divide(double dblNum1, double dblNum2);       
+    }
+}
